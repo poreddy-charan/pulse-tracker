d8f9f697cc218ebfa594a8ce376907e1981d647b
poreddy-charan <saicharanreddypo@gmail.com>
2026-08-16 23:18:34 -0400

    Update CACHE_NAME to sakhi-shell-v4

--- sakhi/sw.js
+++ sakhi/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='sakhi-shell-v3';
+const CACHE_NAME='sakhi-shell-v4';
 const APP_FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
